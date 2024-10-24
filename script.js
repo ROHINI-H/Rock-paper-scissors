03a954225750a4903acbbff73f0b1cc83ca2a489
Rohini <h.rohini19@gmail.com>
2024-10-24 11:28:05 +0530

    Step 2 Completed - getComputerChoice function created

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("HI");
+function getComputerChoice() {
+    let arr = ['Rock', 'paper', 'Scissor']
+    return arr[Math.floor(Math.random() * 3)];
+}
+
+console.log(getComputerChoice());
