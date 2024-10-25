55dd93ebf4ef58a9d4e9536b30d1b77139e2109e
Rohini <h.rohini19@gmail.com>
2024-10-25 10:15:55 +0530

    Step 4 completed - Declared the players score variables

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function getComputerChoice() {
     return arr[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
     let input = prompt('Choose one of number: Rock(1), paper(2), or Scissors(3)', 0);
     if(input <= 3 && input >= 1) {
@@ -14,4 +12,8 @@ function getHumanChoice() {
     }
 }
 
+console.log(getComputerChoice());
 console.log(getHumanChoice())
+
+let humanScore = 0;
+let computerScore = 0;
